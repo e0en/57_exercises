7c8e081557c542369a135b1b4cd948fd8c0aa0ba
Yoonseop Kang <e0engoon@gmail.com>
2016-10-09 17:56:54 +0900

    Separate input prompt, msg generation, output

--- haskell/01/hello_world.hs
+++ haskell/01/hello_world.hs
@@ -1,7 +1,12 @@
 import System.IO
 
-main = do
+prompt = do
   putStr "What is your name? "
   hFlush stdout
-  name <- getLine
-  putStrLn ("Hello, " ++ name ++ ", nice to meet you!")
+  getLine
+
+messageFor name = "Hello, " ++ name ++ ", nice to meet you!"
+
+main = do
+  name <- prompt
+  putStrLn $ messageFor name
