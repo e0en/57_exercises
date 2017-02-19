ebd2d2c9bdfd005c080f757ea7bff510b9e4bfb7
Yoonseop Kang <e0engoon@gmail.com>
2017-02-19 18:28:58 +0900

    Solution to problem 9

--- haskell/09/paint.hs
+++ haskell/09/paint.hs
@@ -9,7 +9,7 @@ ask q = do
     putStrF (q ++ " ")
     getLine
 
-numPattern = "^-?[0-9]+\\.?[0-9]*$"
+natNumPattern = "^[0-9]+$"
 
 bottlesPerSqMeter = 9.0
 calcPaintBottleCount area = ceiling $ (fromIntegral area) / bottlesPerSqMeter
@@ -21,8 +21,18 @@ promptPattern q p = do
     then
         return nStr
     else
-        promptPattern q p
+        do
+            putStrLn "Wrong input"
+            promptPattern q p
+
+promptNatNum q = do
+    s <- promptPattern q natNumPattern
+    return (read s :: Integer)
 
 main = do
-    i <- promptPattern "Number!" numPattern
-    putStrLn $ show (read i :: Int)
+    w <- promptNatNum "Width of your room in meters?"
+    h <- promptNatNum "Height of your room in meters?"
+    let area = w * h
+    let bottles = calcPaintBottleCount area
+    putStrLn $ "You will need to purchase " ++ (show bottles) ++ " liters of"
+    putStrLn $ "paint to cover " ++ (show area) ++ " square meters."
